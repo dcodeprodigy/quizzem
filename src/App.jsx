24ad878a1675b93df7ab59ecae01ef548f9e5c65
A. Joseph <nwodojoseph88@gmail.com>
2025-05-07 22:45:22 +0100

    Added basic protection for routes. This should be enough for normal users to navigate

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import {UserProvider} from "../context/user";
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link, Navigate } from "react-router-dom";
 import SignupPage from "./pages/auth/signup";
 import LoginPage from "./pages/auth/login";
 import Dashboard from "./pages/dashboard";
@@ -8,19 +8,21 @@ import QuizPage from "./pages/quiz";
 import VerifyAccount from "./pages/auth/verify-account";
 
 function App() {
+  const isAuth = localStorage.getItem("token") ? true : false;
+
   return (
     <Router>
       <Routes>
-        <Route path="/signup" element={<SignupPage/>}/>
-        <Route path="/login" element={<LoginPage/>}/>
-        <Route path="/verify-account/:token" element={<VerifyAccount/>}/>
-        <Route path="/dashboard" element={<Dashboard/>}/>
+        <Route path="/signup" element={isAuth ? <Navigate to="/dashboard"/> : <SignupPage/>}/>
+        <Route path="/login" element={isAuth ?  <Navigate to="/dashboard"/> : <LoginPage/>}/>
+        <Route path="/verify-account/:token" element={isAuth ? <Navigate to="/dashboard"/> : <VerifyAccount/>}/>
+        <Route path="/dashboard" element={isAuth ? <Dashboard/> : <Navigate to="/login"/>}/>
         {/* exam mode */}
-        <Route path="/quiz/e/:id" element={<QuizPage isExam={true}/>}/> 
+        <Route path="/quiz/e/:id" element={isAuth ? <QuizPage isExam={true}/> : <Navigate to="/login"/>}/> 
         {/* study mode */}
-        <Route path="/quiz/s/:id" element={<QuizPage isExam={false}/>}/> 
-        <Route path="/" element={<Dashboard />}/>
-        <Route path="*" element={<Dashboard />}/>
+        <Route path="/quiz/s/:id" element={isAuth ? <QuizPage isExam={false}/> : <Navigate to="login"/>}/> 
+        <Route path="/" element={isAuth ? <Dashboard/> : <Navigate to="/login"/>}/>
+        <Route path="*" element={isAuth ? <Dashboard/> : <Navigate to="/login"/>}/>
       </Routes>
     </Router>
   )
